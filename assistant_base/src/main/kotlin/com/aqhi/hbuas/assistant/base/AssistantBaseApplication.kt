9d963bdeec470770b7a1f90fc946ae793fd2bd34
wangzhiyuan <wangzhiyuan@a8sport.com>
2018-02-28 23:43:21 +0800

    init server

---
+++ assistant_base/src/main/kotlin/com/aqhi/hbuas/assistant/base/AssistantBaseApplication.kt
@@ -0,0 +1,11 @@
+package com.aqhi.hbuas.assistant.base
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AssistantBaseApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(AssistantBaseApplication::class.java, *args)
+}
--- assistant_server/src/main/kotlin/com/aqhi/hbuas/assistant/server/AssistantServerApplication.kt
+++ assistant_server/src/main/kotlin/com/aqhi/hbuas/assistant/server/AssistantServerApplication.kt
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class AssistantApplication
+class AssistantServerApplication
+
 
 fun main(args: Array<String>) {
-    SpringApplication.run(AssistantApplication::class.java, *args)
+    SpringApplication.run(AssistantServerApplication::class.java, *args)
 }
