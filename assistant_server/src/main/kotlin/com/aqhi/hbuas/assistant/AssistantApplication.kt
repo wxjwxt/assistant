08288f511cbd7150cf4fa625ecfadd860faffd87
aqhi <wangzhiyuan@a8sport.com>
2018-02-28 19:08:28 +0800

    init

---
+++ assistant_server/src/main/kotlin/com/aqhi/hbuas/assistant/AssistantApplication.kt
@@ -0,0 +1,11 @@
+package com.aqhi.hbuas.assistant
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class AssistantApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(AssistantApplication::class.java, *args)
+}
---
+++ assistant_server/src/test/kotlin/com/aqhi/hbuas/assistant/AssistantApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.aqhi.hbuas.assistant
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class AssistantApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
